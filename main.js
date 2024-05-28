8a4ffcdf809d9487d76266f241349d79d5272609
MeznahAldossari <MeznahAldossari@hotmail.com>
2024-05-28 03:46:22 +0300

    v1

---
+++ main.js
@@ -0,0 +1,8 @@
+function dropList() {
+  let dispalyValue = document.getElementById("dropdown");
+  if (dispalyValue.style.display != "block") {
+    dispalyValue.style.display = "block";
+  } else {
+    dispalyValue.style.display = "none";
+  }
+}
